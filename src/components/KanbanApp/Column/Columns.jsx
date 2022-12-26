6363b5c7811c56359eb5135b39221e65946d9ee1
Yevhenii Sivukhin <fleffydesign@gmail.com>
2022-12-26 21:25:35 +0200

    some changes in columns

--- src/components/KanbanApp/Column/Columns.jsx
+++ src/components/KanbanApp/Column/Columns.jsx
@@ -1,10 +1,13 @@
+import { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { removeColumn, addTask, removeTask } from '../../../store/kanbanSlice'
+import { BiEdit, BiTime } from 'react-icons/bi'
 import { v4 as uuidv4 } from 'uuid'
-import { BiEdit } from 'react-icons/bi'
 
 export const Columns = ({ kanban }) => {
 	const dispatch = useDispatch()
+
+	const [titleEditing, setTitleEditing] = useState(false)
 
 	const deleteColumn = (id) => {
 		dispatch(removeColumn(id))
@@ -28,10 +31,25 @@ export const Columns = ({ kanban }) => {
 			<div>
 				<div className='flex flex-col gap-5 text-white bg-indigo-300 dark:bg-[#202123] rounded-lg w-[350px] p-6'>
 					<div className='rounded-lg p-2 flex items-center justify-between'>
-						<button className='flex items-center hover:opacity-80 transition-all'>
-							<h3 className='font-semibold text-xl'>{kanban.title}</h3>
-							<BiEdit className='ml-2 mt-1 w-[20px] h-[20px]' />
-						</button>
+						{titleEditing ? (
+							<div>
+								<input className='rounded-lg outline-none text-black p-1 px-2 w-[150px] font-semibold mr-3'></input>
+								<button
+									onClick={() => setTitleEditing(false)}
+									className='rounded-lg bg-indigo-400 dark:bg-indigo-700 font-semibold p-2 dark:opacity-40 dark:hover:opacity-100 transition-all'
+								>
+									Confirm
+								</button>
+							</div>
+						) : (
+							<button
+								onClick={() => setTitleEditing(true)}
+								className='flex items-center hover:opacity-80 transition-all overflow-hidden max-w-[230px] outline-none'
+							>
+								<h3 className='font-semibold text-xl'>{kanban.title}</h3>
+								<BiEdit className='ml-2 mt-1 w-[20px] h-[20px]' />
+							</button>
+						)}
 						<button
 							onClick={() => deleteColumn(kanban.id)}
 							className='bg-indigo-400 dark:bg-indigo-700 py-2 px-4 rounded-lg outline-none dark:opacity-40 dark:hover:opacity-100 transition'
@@ -45,8 +63,10 @@ export const Columns = ({ kanban }) => {
 								key={uuidv4()}
 								className='bg-indigo-400 dark:bg-[#2a2c2d] rounded-lg p-4 flex flex-col gap-3 mb-4'
 							>
-								<div className='flex justify-between border-b-2 pb-2'>
-									<h4 className='font-bold text-lg'>{task.taskText}</h4>
+								<div className='flex justify-between border-b-[1px] border-b-white border-opacity-40 pb-2'>
+									<p className='font-bold text-lg overflow-hidden max-w-[230px]'>
+										{task.taskText}
+									</p>
 									<button
 										onClick={() => deleteTask(kanban.id, task.taskID)}
 										className='px-2 rounded-lg outline-none'
@@ -55,10 +75,10 @@ export const Columns = ({ kanban }) => {
 									</button>
 								</div>
 								<div className='text-sm'>
-									<div className='flex flex-col gap-2'>
-										<div>{task.taskTime}</div>
+									<div className='flex items-center gap-1'>
+										<BiTime />
+										{task.taskTime}
 									</div>
-									<div className='pt-2'>ID: {kanban.id}</div>
 								</div>
 							</div>
 						))}
