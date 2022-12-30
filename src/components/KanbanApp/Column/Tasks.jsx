d7032c437db2875e45f2492ed5d4df6e839a799b
Yevhenii Sivukhin <fleffydesign@gmail.com>
2022-12-30 15:39:55 +0200

    initial setup react-beautiful-dnd

--- src/components/KanbanApp/Column/Tasks.jsx
+++ src/components/KanbanApp/Column/Tasks.jsx
@@ -21,52 +21,59 @@ export const Tasks = ({ kanban, task, index }) => {
 	}
 
 	return (
-		<div
-			key={task.taskID}
-			className='bg-indigo-400 dark:bg-[#2a2c2d] rounded-lg p-4 flex flex-col gap-3 mb-4'
-		>
-			<div className='flex justify-between border-b-[1px] border-b-white border-opacity-40 pb-2'>
-				<div>
-					{taskEditing ? (
-						<div>
-							<input
-								value={editedTaskText}
-								onChange={(e) => setEditedTaskText(e.target.value)}
-								className='rounded-lg outline-none text-black dark:bg-[#202123] dark:text-white py-2 px-2 w-[150px] font-semibold mr-3'
-								autoFocus
-							></input>
-							<button
-								onClick={() => {
-									confirmEditTask(kanban.id, task.taskID, editedTaskText)
-									setTaskEditing(false)
-								}}
-								className='rounded-lg bg-indigo-300 dark:bg-indigo-700 font-semibold p-2 dark:opacity-40 dark:hover:opacity-100 transition-all'
-							>
-								Confirm
-							</button>
-						</div>
-					) : (
-						<button
-							onClick={() => setTaskEditing(true)}
-							className='font-bold text-lg overflow-hidden max-w-[230px] outline-none'
-						>
-							{task.taskText}
-						</button>
-					)}
-				</div>
-				<button
-					onClick={() => deleteTask(kanban.id, task.taskID)}
-					className='px-2 rounded-lg outline-none'
+		<Draggable draggableId={task.taskID} index={index}>
+			{(provided) => (
+				<div
+					{...provided.draggableProps}
+					{...provided.dragHandleProps}
+					ref={provided.innerRef}
+					key={task.taskID}
+					className='bg-indigo-400 dark:bg-[#2a2c2d] rounded-lg p-4 flex flex-col gap-3 mb-4'
 				>
-					X
-				</button>
-			</div>
-			<div className='text-sm'>
-				<div className='flex items-center gap-1'>
-					<BiTime />
-					{task.taskTime}
+					<div className='flex justify-between border-b-[1px] border-b-white border-opacity-40 pb-2'>
+						<div>
+							{taskEditing ? (
+								<div>
+									<input
+										value={editedTaskText}
+										onChange={(e) => setEditedTaskText(e.target.value)}
+										className='rounded-lg outline-none text-black dark:bg-[#202123] dark:text-white py-2 px-2 w-[150px] font-semibold mr-3'
+										autoFocus
+									></input>
+									<button
+										onClick={() => {
+											confirmEditTask(kanban.id, task.taskID, editedTaskText)
+											setTaskEditing(false)
+										}}
+										className='rounded-lg bg-indigo-300 dark:bg-indigo-700 font-semibold p-2 dark:opacity-40 dark:hover:opacity-100 transition-all'
+									>
+										Confirm
+									</button>
+								</div>
+							) : (
+								<button
+									onClick={() => setTaskEditing(true)}
+									className='font-bold text-lg overflow-hidden max-w-[230px] outline-none'
+								>
+									{task.taskText}
+								</button>
+							)}
+						</div>
+						<button
+							onClick={() => deleteTask(kanban.id, task.taskID)}
+							className='px-2 rounded-lg outline-none'
+						>
+							X
+						</button>
+					</div>
+					<div className='text-sm'>
+						<div className='flex items-center gap-1'>
+							<BiTime />
+							{task.taskTime}
+						</div>
+					</div>
 				</div>
-			</div>
-		</div>
+			)}
+		</Draggable>
 	)
 }
--- src/components/KanbanApp/Kanban.jsx
+++ src/components/KanbanApp/Kanban.jsx
@@ -7,16 +7,20 @@ import { useSelector } from 'react-redux'
 export const Kanban = () => {
 	const kanbanData = useSelector((state) => state.kanban.kanbanData)
 
-	const onDragEnd = () => {}
+	const onDragEnd = () => {
+		// TODO
+	}
 
 	return (
 		<div className='p-6 px-[1.5rem] xl:px-[7.5rem] mt-[50px]'>
-			<AddColumn />
-			<div className='flex gap-3 overflow-auto pb-3'>
-				{kanbanData.map((kanban) => (
-					<Columns key={kanban.id} kanban={kanban} />
-				))}
-			</div>
+			<DragDropContext onDragEnd={() => onDragEnd()}>
+				<AddColumn />
+				<div className='flex gap-3 overflow-auto pb-3'>
+					{kanbanData.map((kanban) => (
+						<Columns key={kanban.id} kanban={kanban} />
+					))}
+				</div>
+			</DragDropContext>
 		</div>
 	)
 }
