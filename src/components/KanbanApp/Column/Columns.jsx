2287be7305f5bba31edaf031b456d50d63d4cc0b
Yevhenii Sivukhin <fleffydesign@gmail.com>
2022-12-30 17:04:19 +0200

    some changes

--- src/components/KanbanApp/Column/Columns.jsx
+++ src/components/KanbanApp/Column/Columns.jsx
@@ -5,7 +5,6 @@ import {
 	addTask,
 	editColumnTitle,
 } from '../../../store/kanbanSlice'
-import { Droppable } from 'react-beautiful-dnd'
 import { Tasks } from './Tasks'
 
 import { BiEdit } from 'react-icons/bi'
@@ -38,67 +37,53 @@ export const Columns = ({ kanban }) => {
 	}
 
 	return (
-		<div>
-			<Droppable droppableId={kanban.id}>
-				{(provided) => (
-					<div ref={provided.innerRef} {...provided.droppableProps}>
-						<div className='flex flex-col gap-5 text-white bg-indigo-300 dark:bg-[#202123] rounded-lg w-[350px] p-6'>
-							<div className='rounded-lg p-2 flex items-center justify-between'>
-								{titleEditing ? (
-									<div>
-										<input
-											value={editedColumnTitle}
-											onChange={(e) => setEditedColumnTitle(e.target.value)}
-											className='rounded-lg outline-none text-black dark:bg-[#2a2c2d] dark:text-white py-2 px-2 w-[150px] font-semibold mr-3'
-											autoFocus
-										></input>
-										<button
-											onClick={() => {
-												confirmNewColumnTitle(kanban.id, editedColumnTitle),
-													setTitleEditing(false)
-											}}
-											className='rounded-lg bg-indigo-400 dark:bg-indigo-700 font-semibold p-2 dark:opacity-40 dark:hover:opacity-100 transition-all'
-										>
-											Confirm
-										</button>
-									</div>
-								) : (
-									<button
-										onClick={() => setTitleEditing(true)}
-										className='flex items-center hover:opacity-80 transition-all overflow-hidden max-w-[230px] outline-none'
-									>
-										<h3 className='font-semibold text-xl'>{kanban.title}</h3>
-										<BiEdit className='ml-2 mt-1 w-[20px] h-[20px]' />
-									</button>
-								)}
-								<button
-									onClick={() => deleteColumn(kanban.id)}
-									className='bg-indigo-400 dark:bg-indigo-700 py-2 px-4 rounded-lg outline-none dark:opacity-40 dark:hover:opacity-100 transition'
-								>
-									X
-								</button>
-							</div>
-							<div>
-								{kanban.tasks.map((task, index) => (
-									<Tasks
-										key={task.taskID}
-										kanban={kanban}
-										task={task}
-										index={index}
-									/>
-								))}
-							</div>
-							<button
-								onClick={() => addNewTask(kanban.id, 'Task Text')}
-								className='bg-indigo-400 dark:bg-indigo-700 rounded-lg p-2 opacity-50 hover:opacity-100 transition-all'
-							>
-								Add
-							</button>
-						</div>
-						{provided.placeholder}
+		<div className='flex flex-col gap-5 text-white bg-indigo-300 dark:bg-[#202123] rounded-lg w-[350px] p-6'>
+			<div className='rounded-lg p-2 flex items-center justify-between'>
+				{titleEditing ? (
+					<div>
+						<input
+							value={editedColumnTitle}
+							onChange={(e) => setEditedColumnTitle(e.target.value)}
+							className='rounded-lg outline-none text-black dark:bg-[#2a2c2d] dark:text-white py-2 px-2 w-[150px] font-semibold mr-3'
+							autoFocus
+						></input>
+						<button
+							onClick={() => {
+								confirmNewColumnTitle(kanban.id, editedColumnTitle),
+									setTitleEditing(false)
+							}}
+							className='rounded-lg bg-indigo-400 dark:bg-indigo-700 font-semibold p-2 dark:opacity-40 dark:hover:opacity-100 transition-all'
+						>
+							Confirm
+						</button>
 					</div>
+				) : (
+					<button
+						onClick={() => setTitleEditing(true)}
+						className='flex items-center hover:opacity-80 transition-all overflow-hidden max-w-[230px] outline-none'
+					>
+						<h3 className='font-semibold text-xl'>{kanban.title}</h3>
+						<BiEdit className='ml-2 mt-1 w-[20px] h-[20px]' />
+					</button>
 				)}
-			</Droppable>
+				<button
+					onClick={() => deleteColumn(kanban.id)}
+					className='bg-indigo-400 dark:bg-indigo-700 py-2 px-4 rounded-lg outline-none dark:opacity-40 dark:hover:opacity-100 transition'
+				>
+					X
+				</button>
+			</div>
+			<div>
+				{kanban.tasks.map((task, index) => (
+					<Tasks key={task.taskID} kanban={kanban} task={task} />
+				))}
+			</div>
+			<button
+				onClick={() => addNewTask(kanban.id, 'Task Text')}
+				className='bg-indigo-400 dark:bg-indigo-700 rounded-lg p-2 opacity-50 hover:opacity-100 transition-all'
+			>
+				Add
+			</button>
 		</div>
 	)
 }
