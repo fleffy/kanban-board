3e27b0d550826bf78e74a009c6e5d7b4375c3ad4
Yevhenii Sivukhin <fleffydesign@gmail.com>
2023-01-15 14:21:38 +0200

    some changes

--- src/components/KanbanApp/Archive/Archive.jsx
+++ src/components/KanbanApp/Archive/Archive.jsx
@@ -1,22 +1,29 @@
 import { useState } from 'react'
 import { DropToArchive } from './DropToArchive'
 import { ArchiveModal } from './ArchiveModal'
+import { DragDropContext } from '@hello-pangea/dnd'
 
 export const Archive = () => {
 	const [openModal, setOpenModal] = useState(false)
 
+	const onDragEnd = () => {
+		// todo
+	}
+
 	return (
-		<div>
-			<div className='flex gap-5 transition-all'>
-				<button
-					onClick={() => setOpenModal(true)}
-					className='text-white p-2 px-5 rounded-lg font-semibold bg-indigo-500 dark:bg-indigo-700'
-				>
-					Archive
-				</button>
-				<DropToArchive />
+		<DragDropContext onDragEnd={onDragEnd()}>
+			<div>
+				<div className='flex gap-5 transition-all'>
+					<button
+						onClick={() => setOpenModal(true)}
+						className='text-white p-2 px-5 rounded-lg font-semibold bg-indigo-500 dark:bg-indigo-700'
+					>
+						Archive
+					</button>
+					<DropToArchive />
+				</div>
+				{openModal ? <ArchiveModal setOpenModal={setOpenModal} /> : <div></div>}
 			</div>
-			{openModal ? <ArchiveModal setOpenModal={setOpenModal} /> : <div></div>}
-		</div>
+		</DragDropContext>
 	)
 }
